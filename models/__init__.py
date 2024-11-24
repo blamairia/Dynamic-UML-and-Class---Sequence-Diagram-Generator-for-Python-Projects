d3f006ad4a8cb6a01ea113f16667063079a87e88
BLAMAIRIA <blamairia@gmail.com>
2024-11-24 22:14:27 +0100

    init.py

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+from models.person import Person
+from models.student import Student
+from models.course import Course
+from models.professor import Professor
+
+__all__ = ["Person", "Student", "Course", "Professor"]
